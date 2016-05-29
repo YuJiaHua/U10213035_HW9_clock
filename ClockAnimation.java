c1a8a480395719e676bb2f51dd24fa91bfef560a
YuJiaHua <k408202002@yahoo.com.tw>
2016-05-30 00:27:33 +0800

    Update ClockAnimation.java
    Add two buttons to control the animation drive

--- ClockAnimation.java
+++ ClockAnimation.java
@@ -4,21 +4,21 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Paint;
 import javafx.util.Duration;
 
 public class ClockAnimation extends Application {
   private static final Paint WHITE = null;
-private static final Paint BLACK = null;
-
+  private static final Paint BLACK = null;
+  
 @Override // Override the start method in the Application class
   public void start(Stage primaryStage) {
+	Stage stage = new Stage();
     ClockPane clock = new ClockPane(); // Create a clock
-          
+    ControlClock controlClock = new ControlClock();
+
     // Create a handler for animation
     EventHandler<ActionEvent> eventHandler = e -> {
       clock.setCurrentTime(); // Set a new clock time
@@ -30,11 +30,32 @@ private static final Paint BLACK = null;
     animation.setCycleCount(Timeline.INDEFINITE);
     animation.play(); // Start animation
        
+    //Add button b1
+    Button b1 = new Button("Start");
+    b1.setLayoutX(40);
+    b1.setLayoutY(10);
+    //Add start animation
+    b1.setOnAction(e -> animation.play());
+    
+	  //Add button b2
+    Button b2 = new Button("Stop");
+    b2.setLayoutX(160);
+    b2.setLayoutY(10);
+    //Add stop animation
+    b2.setOnAction(e -> animation.pause());
+    
+    controlClock.getChildren().add(b1);
+    controlClock.getChildren().add(b2);
+    
     // Create a scene and place it in the stage
     Scene scene = new Scene(clock, 250, 250);
+    Scene scene2 = new Scene(controlClock, 250, 50);
     primaryStage.setTitle("ClockAnimation"); // Set the stage title
+    stage.setTitle("ControlClock");
     primaryStage.setScene(scene); // Place the scene in the stage
+    stage.setScene(scene2);
     primaryStage.show(); // Display the stage
+    stage.show();
   }
 
   /**
